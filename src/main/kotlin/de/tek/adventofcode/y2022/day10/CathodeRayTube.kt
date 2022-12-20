91cc9c351581c64936b18b2f326a4b40c8af65b4
thomas <thumas@mailbox.org>
2022-12-20 18:03:02 +0100

    Completed day 10, part 2.

--- src/main/kotlin/de/tek/adventofcode/y2022/day10/CathodeRayTube.kt
+++ src/main/kotlin/de/tek/adventofcode/y2022/day10/CathodeRayTube.kt
@@ -1,35 +1,56 @@
 package de.tek.adventofcode.y2022.day10
 
 import de.tek.adventofcode.y2022.util.readInputLines
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.function.Predicate
+import java.util.function.Supplier
 
-class Cpu(private var register: Int = 1) {
-    private var currentCycle = 0
+typealias Register = AtomicInteger
 
-    private val cycleListeners = mutableListOf<CycleListener>()
+class Device constructor(
+    private val clock: Clock,
+    private val register: Register = Register(1),
+    private val tube: CathodeRayTube = CathodeRayTube(register::get),
+    private val cpu: Cpu = Cpu(register),
+    program: List<Instruction>
+) {
+    private val instructions = program.toMutableList()
+
+    constructor(instructions: List<Instruction>) : this(clock = Clock(), program = instructions)
+
+    init {
+        clock.registerCycleListener(tube::draw)
+        clock.registerCycleListener {
+            instructions.firstOrNull()?.also { cpu.execute(it) }
+        }
+    }
+
+    fun run() {
+        while (instructions.isNotEmpty()) {
+            clock.startCycle()
+            instructions.removeIf { it.isCompleted() }
+        }
+    }
+
+    fun getSignalStrength() = register.get() * clock.currentCycle
+
+    fun showScreen() = tube.visualize()
+}
+
+class Clock {
+    var currentCycle = 0
+        private set
 
     private class CycleListener(val cyclePredicate: Predicate<Int>, val action: Runnable)
 
-    fun getSignalStrength() = register * currentCycle
+    private val cycleListeners = mutableListOf<CycleListener>()
+
+    fun registerCycleListener(action: Runnable) = registerCycleListener({ true }, action)
 
     fun registerCycleListener(cyclePredicate: Predicate<Int>, action: Runnable) =
         cycleListeners.add(CycleListener(cyclePredicate, action))
 
-    fun execute(instruction: Instruction) {
-        when (instruction) {
-            is Instruction.Noop -> {
-                startCycle()
-            }
-
-            is Instruction.AddX -> {
-                startCycle()
-                startCycle()
-                register += instruction.value
-            }
-        }
-    }
-
-    private fun startCycle() {
+    fun startCycle() {
         currentCycle++
         informListeners()
     }
@@ -39,14 +60,60 @@ class Cpu(private var register: Int = 1) {
     }
 }
 
-sealed class Instruction {
-    object Noop : Instruction()
-    class AddX(val value: Int) : Instruction()
+class Cpu(private var register: Register) {
+    fun execute(instruction: Instruction) {
+        instruction.spendCycle()
+
+        if (instruction.isCompleted()) {
+            when (instruction) {
+                is Instruction.Noop -> {}
+                is Instruction.AddX -> register.getAndAdd(instruction.value)
+            }
+        }
+    }
+
+}
+
+class CathodeRayTube(private val registerReader: Supplier<Int>) {
+    private val screen = Array(6) { Array(40) { ' ' } }
+
+    private var currentPixel = 0
+
+    fun draw() {
+        val linePosition = currentPixel.mod(40)
+
+        val register = registerReader.get()
+        val spritePositions = register - 1..register + 1
+
+        screen[currentPixel / 40][linePosition] = if (linePosition in spritePositions) '#' else '.'
+
+        currentPixel = (currentPixel + 1).mod(40 * 6)
+    }
+
+    override fun toString(): String {
+        return "CathodeRayTube(screen=${screen.contentToString()}, currentPixel=$currentPixel)"
+    }
+
+    fun visualize() = screen.joinToString("\n") { it.joinToString("") }
+}
+
+sealed class Instruction(cycles: Int) {
+    private var remainingCycles = cycles
+
+    fun spendCycle() {
+        remainingCycles--
+    }
+
+    fun isCompleted() = remainingCycles <= 0
+
+    class Noop : Instruction(1)
+    class AddX(val value: Int) : Instruction(2)
+
     companion object {
         fun parseFrom(string: String): Instruction {
             val splitString = string.split(" ")
             return when (splitString[0]) {
-                Noop::class.simpleName?.lowercase() -> Noop
+                Noop::class.simpleName?.lowercase() -> Noop()
                 AddX::class.simpleName?.lowercase() -> AddX(splitString[1].toInt())
                 else -> throw IllegalArgumentException("Unknown instruction: $string.")
             }
@@ -54,20 +121,29 @@ sealed class Instruction {
     }
 }
 
-
 fun main() {
     val input = readInputLines(Cpu::class)
 
     fun part1(input: List<String>): Int {
-        val cpu = Cpu()
+        val clock = Clock()
+        val device = Device(clock, program = input.map(Instruction::parseFrom))
 
         val signalStrengths = mutableListOf<Int>()
-        cpu.registerCycleListener({ (it - 20).mod(40) == 0 }) { signalStrengths.add(cpu.getSignalStrength()) }
+        clock.registerCycleListener({ (it - 20).mod(40) == 0 }) { signalStrengths.add(device.getSignalStrength()) }
 
-        input.map(Instruction::parseFrom).forEach { cpu.execute(it) }
+        device.run()
 
         return signalStrengths.sum()
     }
 
+    fun part2(input: List<String>): String {
+        val device = Device(input.map(Instruction::parseFrom))
+
+        device.run()
+
+        return device.showScreen()
+    }
+
     println("The sum of the signal strengths is ${part1(input)}.")
+    println("The screen shows:\n${part2(input)}")
 }
--- src/test/kotlin/de/tek/adventofcode/y2022/day10/CathodeRayTubeTest.kt
+++ src/test/kotlin/de/tek/adventofcode/y2022/day10/CathodeRayTubeTest.kt
@@ -7,18 +7,19 @@ import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 
 class CpuTest : StringSpec({
-    "Given new CPU, getSignalStrength returns 0." {
-        Cpu().getSignalStrength() shouldBe 0
+
+    "Given new device, getSignalStrength returns 0." {
+        Device(listOf()).getSignalStrength() shouldBe 0
     }
 
-    "Given CPU and execute noop, getSignalStrength returns 1." {
-        val cpu = Cpu()
-        cpu.execute(Instruction.Noop)
+    "Given device and execute noop, getSignalStrength returns 2." {
+        val device = Device(listOf(Instruction.Noop()))
+        device.run()
 
-        cpu.getSignalStrength() shouldBe 1
+        device.getSignalStrength() shouldBe 1
     }
 
-    "Given CPU and execute addx with a value, getSignalStrength returns that value plus 1 doubled." {
+    "When executing addx with a value, the signal strengths collected at during the cycles are 1 and 2." {
         forAll(
             row(1),
             row(2),
@@ -26,14 +27,20 @@ class CpuTest : StringSpec({
             row(-1),
             row(-113)
         ) { value ->
-            val cpu = Cpu()
-            cpu.execute(Instruction.AddX(value))
+            val clock = Clock()
+            val signalStrengths = mutableListOf<Int>()
+            var device: Device? = null
+            // first register this test listener, so we get the signal strength before finishing the cycle
+            clock.registerCycleListener { signalStrengths.add(device?.getSignalStrength() ?: 0) }
+            device = Device(clock = clock, program = listOf(Instruction.AddX(value)))
 
-            cpu.getSignalStrength() shouldBe 2 * (1 + value)
+            device!!.run()
+
+            signalStrengths shouldContainExactly listOf(1, 2)
         }
     }
 
-    "When executing addx with a value, the collected signal strengths are 1 and 2." {
+    "When executing addx with a value, the signal strengths collected at the end of the cycles are the value plus one doubled." {
         forAll(
             row(1),
             row(2),
@@ -41,14 +48,14 @@ class CpuTest : StringSpec({
             row(-1),
             row(-113)
         ) { value ->
-            val cpu = Cpu()
-
+            val clock = Clock()
             val signalStrengths = mutableListOf<Int>()
-            cpu.registerCycleListener({ true }) { signalStrengths.add(cpu.getSignalStrength()) }
+            val device = Device(clock = clock, program = listOf(Instruction.AddX(value)))
+            clock.registerCycleListener { signalStrengths.add(device.getSignalStrength()) }
 
-            cpu.execute(Instruction.AddX(value))
+            device.run()
 
-            signalStrengths shouldContainExactly listOf(1, 2)
+            signalStrengths shouldContainExactly listOf(1, 2*(1+value))
         }
     }
 
@@ -61,16 +68,90 @@ class CpuTest : StringSpec({
             row(21, listOf(10,20)),
             row(56, listOf(10,20,30,40,50)),
         ) { numberOfNoOps, expectedSignalStrengths ->
-            val cpu = Cpu()
+            val clock = Clock()
+            val device = Device(clock = clock, program = List(numberOfNoOps) { Instruction.Noop() })
 
             val signalStrengths = mutableListOf<Int>()
-            cpu.registerCycleListener({ it.mod(10) == 0 }) { signalStrengths.add(cpu.getSignalStrength()) }
+            clock.registerCycleListener({ it.mod(10) == 0 }) { signalStrengths.add(device.getSignalStrength()) }
 
-            repeat(numberOfNoOps) { cpu.execute(Instruction.Noop) }
+            device.run()
 
             signalStrengths shouldContainExactly expectedSignalStrengths
         }
     }
+})
 
+class CathodeRayTubeTest : StringSpec({
+    "Given the register is -2 and drawing all pixels, then visualize returns an empty screen." {
+        val cathodeRayTube = CathodeRayTube { -2 }
 
+        repeat(40*6) {
+            cathodeRayTube.draw()
+        }
+
+        cathodeRayTube.visualize() shouldBe """
+            ........................................
+            ........................................
+            ........................................
+            ........................................
+            ........................................
+            ........................................
+        """.trimIndent()
+    }
+    
+    "Given the register is 1 and drawing all pixels, then visualize returns a screen with the sprite on the left border." {
+        val cathodeRayTube = CathodeRayTube { 1 }
+
+        repeat(40*6) {
+            cathodeRayTube.draw()
+        }
+
+        cathodeRayTube.visualize() shouldBe """
+            ###.....................................
+            ###.....................................
+            ###.....................................
+            ###.....................................
+            ###.....................................
+            ###.....................................
+        """.trimIndent()
+    }
+    
+    "Given the register progressing in 1-steps and drawing all pixels, then visualize returns a full first line." {
+        val register = Register(1)
+        val cathodeRayTube = CathodeRayTube { register.getAndIncrement() }
+
+        repeat(40*6) {
+            cathodeRayTube.draw()
+        }
+
+        cathodeRayTube.visualize() shouldBe """
+            ########################################
+            ........................................
+            ........................................
+            ........................................
+            ........................................
+            ........................................
+        """.trimIndent()
+    }
+
+    "Given the register progressing in 1-steps, resetting after 40 and drawing all pixels, then visualize returns a full screen." {
+        val register = Register(0)
+        val cathodeRayTube = CathodeRayTube {
+            register.getAndIncrement()
+            register.compareAndExchange(40, 0)
+        }
+
+        repeat(40*6) {
+            cathodeRayTube.draw()
+        }
+
+        cathodeRayTube.visualize() shouldBe """
+            ########################################
+            ########################################
+            ########################################
+            ########################################
+            ########################################
+            ########################################
+        """.trimIndent()
+    }
 })
